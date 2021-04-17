995b5229880a6915754957c7c844b5c56dcbbe3e
Moonriderx <luben1993@abv.bg>
2021-04-17 14:20:35 +0300

    Refactor
    Refactored some scripts, added more functionalities

---
+++ Scripts/Input Devices/VirtualInputManager.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+namespace Moonrider
+{
+    public class VirtualInputManager : Singleton<VirtualInputManager>
+    {
+        
+        public bool moveRight;
+        public bool moveLeft;
+    }
+}
