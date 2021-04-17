d1293cc18bcf4137726d6bd4c6846fadd89e1792
Moonriderx <luben1993@abv.bg>
2021-04-17 12:38:05 +0300

    Added a few basic scripts
    Moving character scripts

---
+++ Scripts/KeyboardInput.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+namespace Moonrider
+{
+
+
+    public class KeyboardInput : MonoBehaviour
+    {
+         
+        void Update()
+        {
+            if (Input.GetKey(KeyCode.D))
+            {
+                VirtualInputManager.Instance.moveRight = true;
+            }
+            else
+            {
+                VirtualInputManager.Instance.moveRight = false;
+            }
+
+            if (Input.GetKey(KeyCode.A))
+            {
+                VirtualInputManager.Instance.moveLeft = true;
+            }
+            else
+            {
+                VirtualInputManager.Instance.moveLeft = false;
+            }
+        }
+    }
+}
