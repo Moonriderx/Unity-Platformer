f94b456aca6c6972d5a6a22bf1a18015c1ba8fc0
Moonriderx <luben1993@abv.bg>
2021-04-17 21:15:37 +0300

    Separated Move logic in different states
    Removed Move Logic from characterControl script and separated it using FSM
    (animator state machine)

--- Scripts/CharacterControl.cs
+++ Scripts/CharacterControl.cs
@@ -16,36 +16,7 @@ namespace Moonrider
 
         // Since this is 2.5d we are going to mainly playing around "y" axis. 0 means we are going forward --> 180 means we are going backwards <--
        
-        void Update()
-        {
-
-            if (VirtualInputManager.Instance.moveLeft && VirtualInputManager.Instance.moveRight)
-            {
-                animator.SetBool(TransitionParameter.Move.ToString(), false); // if the character is not moving
-                return; // this fix the "bug" where if u press A and D at the same time the character rotation "glitch". If u press the both buttons the character will just stops
-            }
-
-            if (!VirtualInputManager.Instance.moveLeft && !VirtualInputManager.Instance.moveRight) // if not pressing anything
-            {
-                animator.SetBool(TransitionParameter.Move.ToString(), false);
-            }
-
-                 if (VirtualInputManager.Instance.moveRight)
-            {
-                this.gameObject.transform.Translate(Vector3.forward * Speed * Time.deltaTime); // the script is attached to this game object -> will move forward. Time.deltaTime will be used to negate the
-                                                                                             // time difference between each frame
-                this.gameObject.transform.rotation = Quaternion.Euler(0f, 0f, 0f); // Character turns (rotates) toward the running direction
-                animator.SetBool(TransitionParameter.Move.ToString(), true); // if the character is moving
-
-            }
-
-            if (VirtualInputManager.Instance.moveLeft)
-            {
-                this.gameObject.transform.Translate(Vector3.forward * Speed * Time.deltaTime); // the script is attached to this game object -> will move backwards.
-                this.gameObject.transform.rotation = Quaternion.Euler(0f, 180f, 0f); // Character turns (rotates) toward the running direction
-                animator.SetBool(TransitionParameter.Move.ToString(), true);
-            }
-        }
+      
 
         public void ChangeMaterial()
         {
